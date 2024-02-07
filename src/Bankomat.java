446f538084a5ea08e68d31d04c5eb30087e330b5
Alexsej <aleksejshmakov7@gmail.com>
2024-02-07 22:54:09 +0300

    Universal code

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Bankomat {
     int money;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public int password(Human human) {
+        int popitka = 0;
+        System.out.println("Введите пароль");
+        int pass = scanner.nextInt();
+        for (int i = 0; i <= 1; i++) {
+            if (pass != human.card.password) {
+                System.out.println("Вы ввели неверный пароль, попробуйте еще раз!");
+                pass = scanner.nextInt();
+            }
+            popitka = popitka + i;
+
+
+            if (pass == human.card.password) {
+                System.out.println("Вы ввели верный пароль! Добро пожаловать " + human.name);
+                return 1;
+            }
+        }
+        System.out.println("Вы превысили лимит попыток, ваша карта временно заблокирована.");
+        return 0;
+    }
+
 
     public void datMoney(Human human, int count) {
         if (human.card.balance < count) { //500 <= 0
@@ -8,6 +34,8 @@ public class Bankomat {
         }
         human.card.balance += count;
         money -= count;
+        System.out.println("Баланс банкомата после выдачи средств - " + money);
+        System.out.println("Баланс карты после снятия средств - " + human.card.balance);
     }
 
     public void zabratMoney(Human human, int count) {
@@ -17,5 +45,7 @@ public class Bankomat {
         }
         human.card.balance -= count;
         money += count;
+        System.out.println("Баланс банкомата после внесения средств - " + money);
+        System.out.println("Баланс карты после списания средств - " + human.card.balance);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        Human human = new Human();
-        human.name = "Егор";
-        Human human1 = new Human();
-        human1.name = "Антон";
+        Scanner scanner = new Scanner(System.in);
+        Human Egor = new Human();
+        Egor.name = "Егор";
+        Human Anton = new Human();
+        Anton.name = "Антон";
 
         Card mastercard = new Card();
         mastercard.balance = 1000;
@@ -15,63 +17,31 @@ public class Main {
         visa.balance = 1000;
         visa.password = 1234;
 
-        human1.card = visa;
-        human.card = mastercard;
+        Anton.card = visa;
+        Egor.card = mastercard;
 
 
         Bankomat bankomat = new Bankomat();
         bankomat.money = 10000;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите пожалуйста ПИН-код от Вашей карты!");
-        int pass = scanner.nextInt();
-        switch (pass) {
-            case (1234):
-                System.out.println("Здравствуйте, Антон!");
-                System.out.println("Выбирите дальнейшую операцию, где \n1 - это снятие средств!\n2 - это пополнение счета! ");
-                int operations = scanner.nextInt();
-                switch (operations) {
-                    case (1):
-                        System.out.println("Введите сумму, которую хотите снять - ");
-                        bankomat.datMoney(human1, scanner.nextInt());
-                        System.out.println("Bank posle snytia: " + bankomat.money);
-                        System.out.println(human1.card.balance);
-                        break;
-                    case (2): {
-                        System.out.println("Введите сумму, которую хотите положить - ");
-                        bankomat.zabratMoney(human1, scanner.nextInt());
-                        System.out.println("Bank posle popolnenia: " + bankomat.money);
-                        System.out.println("Егор, Ваш баланс карты теперь " + human1.card.balance);
-                        break;
-                    }
-                    default:
-                        System.out.println("Такого номера операции не существует, попробуйте снова!");
+        System.out.println("Выберите операцию где 1 - снять, а 2 - положить.");
+        int operations = scanner.nextInt();
+        switch (operations) {
+            case (1): {
+                int pass = bankomat.password(Anton);
+                if (pass == 1) {
+                    System.out.println("Введите сумму оперции по снятию средств.");
+                    bankomat.datMoney(Anton, scanner.nextInt());
                 }
-                break;
-            case (4321):
-                System.out.println("Здравствуйте, Егор!");
-                System.out.println("Выбирите дальнейшую операцию, где \n1 - это снятие средств!\n2 - это пополнение счета! ");
-                int operation = scanner.nextInt();
-                switch (operation) {
-                    case (1):
-                        System.out.println("Введите сумму, которую хотите снять - ");
-                        bankomat.datMoney(human, scanner.nextInt());
-                        System.out.println("Bank posle snytia: " + bankomat.money);
-                        System.out.println(human.card.balance);
-                        break;
-                    case (2): {
-                        System.out.println("Введите сумму, которую хотите положить - ");
-                        bankomat.zabratMoney(human, scanner.nextInt());
-                        System.out.println("Bank posle popolnenia: " + bankomat.money);
-                        System.out.println(human.card.balance);
-                        break;
-                    }
-                    default:
-                        System.out.println("Такого номера операции не существует, попробуйте снова!");
+            }
+            break;
+            case (2): {
+                int pass = bankomat.password(Anton);
+                if (pass == 1) {
+                    System.out.println("Введите сумму оперции по внесению средств.");
+                    bankomat.zabratMoney(Anton, scanner.nextInt());
                 }
-                break;
-            default:
-                System.out.println("Пин-код введен не верно, повторите ввод!");
+            }
+            break;
         }
-
     }
 }
